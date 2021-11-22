5a6993e374ae35bd672573be24737ea45b7b2cdc
cyrcus-buidls <cameron@cyrcus.net>
2021-11-21 21:09:19 -0800

    Added rules for fighting the boss

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -36,6 +36,16 @@ contract MyEpicGame is ERC721 {
   // We create a mapping from the nft's tokenId => that NFTs attributes.
   mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
 
+  struct BigBoss {
+    string name;
+    string imageURI;
+    uint hp;
+    uint maxHp;
+    uint attackDamage;
+  }
+
+  BigBoss public bigBoss;
+
   // A mapping from an address => the NFTs tokenId. Gives me an ez way
   // to store the owner of the NFT and reference it later.
   mapping(address => uint256) public nftHolders;
@@ -44,13 +54,25 @@ contract MyEpicGame is ERC721 {
     string[] memory characterNames,
     string[] memory characterImageURIs,
     uint[] memory characterHp,
-    uint[] memory characterAttackDmg
-    // Below, you can also see I added some special identifier symbols for our NFT.
-    // This is the name and symbol for our token, ex Ethereum and ETH. I just call mine
-    // Heroes and HERO. Remember, an NFT is just a token!
+    uint[] memory characterAttackDmg,
+    string memory bossName, // These new variables would be passed in via run.js or deploy.js.
+    string memory bossImageURI,
+    uint bossHp,
+    uint bossAttackDamage
   )
     ERC721("ETHFighers-001", "EF001")
   {
+    // Initialize the boss. Save it to our global "bigBoss" state variable.
+    bigBoss = BigBoss({
+      name: bossName,
+      imageURI: bossImageURI,
+      hp: bossHp,
+      maxHp: bossHp,
+      attackDamage: bossAttackDamage
+    });
+
+  console.log("Done initializing boss %s w/ HP %s, img %s", bigBoss.name, bigBoss.hp, bigBoss.imageURI);
+  
     for(uint i = 0; i < characterNames.length; i += 1) {
       defaultCharacters.push(CharacterAttributes({
         characterIndex: i,
@@ -100,6 +122,44 @@ contract MyEpicGame is ERC721 {
     // Increment the tokenId for the next person that uses it.
     _tokenIds.increment();
   }
+
+  function attackBoss() public {
+    // Get the state of the player's NFT.
+    uint256 nftTokenIdOfPlayer = nftHolders[msg.sender];
+    CharacterAttributes storage player = nftHolderAttributes[nftTokenIdOfPlayer];
+    console.log("\nPlayer w/ character %s about to attack. Has %s HP and %s AD", player.name, player.hp, player.attackDamage);
+    console.log("Boss %s has %s HP and %s AD", bigBoss.name, bigBoss.hp, bigBoss.attackDamage);
+
+    // Make sure the player has more than 0 HP.
+    require (
+      player.hp > 0,
+      "Error: character must have HP to attack boss."
+    );
+
+    // Make sure the boss has more than 0 HP.
+    require (
+      bigBoss.hp > 0,
+      "Error: boss must have HP to attack boss."
+    );
+
+    // Allow player to attack boss.
+    if (bigBoss.hp < player.attackDamage) {
+      bigBoss.hp = 0;
+    } else {
+      bigBoss.hp = bigBoss.hp - player.attackDamage;
+    }
+
+    // Allow boss to attack player.
+    if (player.hp < bigBoss.attackDamage) {
+      player.hp = 0;
+    } else {
+      player.hp = player.hp - bigBoss.attackDamage;
+    }
+  
+  // Console for ease.
+  console.log("Player attacked boss. New boss hp: %s", bigBoss.hp);
+  console.log("Boss attacked player. New player hp: %s\n", player.hp);
+}
 
   function tokenURI(uint256 _tokenId) public view override returns (string memory) {
   CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
--- scripts/run.js
+++ scripts/run.js
@@ -6,7 +6,11 @@ const main = async () => {
     "https://i.imgur.com/LANuDHg.jpeg", 
     "https://i.imgur.com/QHROqrw.jpeg"],
     [400, 250, 150],                    // HP values
-    [50, 100, 200]                       // Attack damage values
+    [50, 100, 200],                       // Attack damage values
+    "Moloch of Course Boredom", // Boss name
+    "https://i.imgur.com/VtRuALl.png", // Boss image
+    10000, // Boss hp
+    50 // Boss attack damage
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
@@ -14,12 +18,18 @@ const main = async () => {
   let txn;
   // We only have three characters.
   // an NFT w/ the character at index 2 of our array.
-  txn = await gameContract.mintCharacterNFT(2);
+  txn = await gameContract.mintCharacterNFT(1);
+  await txn.wait();
+
+  txn = await gameContract.attackBoss();
+  await txn.wait();
+
+  txn = await gameContract.attackBoss();
   await txn.wait();
 
   // Get the value of the NFT's URI.
-  let returnedTokenUri = await gameContract.tokenURI(1);
-  console.log("Token URI:", returnedTokenUri);
+  // let returnedTokenUri = await gameContract.tokenURI(1);
+  // console.log("Token URI:", returnedTokenUri);
 };
 
 const runMain = async () => {
